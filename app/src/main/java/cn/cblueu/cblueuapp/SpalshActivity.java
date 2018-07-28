a4a646ecc5a0b18537882165e41e45187d117bc0
zerowang <925862192@qq.com>
2018-07-28 13:31:27 +0800

    增加公交服务功能，待添加动画

--- app/src/main/java/cn/cblueu/cblueuapp/SpalshActivity.java
+++ app/src/main/java/cn/cblueu/cblueuapp/SpalshActivity.java
@@ -1,7 +1,6 @@
 package cn.cblueu.cblueuapp;
 
 import android.app.Activity;
-import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,8 +9,8 @@ public class SpalshActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_activity_spalsh);
-        Intent intent = new Intent(this,MainActivity.class);
+        setContentView(R.layout.activity_spalsh);
+        Intent intent = new Intent(this,BusActivity.class);
         startActivity(intent);
     }
 }
